ac53aa9ce16a2a50128a617778efda6093451d54
impxharsh <20harshanand24@gmail.com>
2025-10-24 00:03:40 +0530

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,7 @@ function App() {
     
     // Create the new, updated list
     const updatedContacts = [newContactWithId, ...allContacts];
+    
     // Update state
     setAllContacts(updatedContacts);
     
@@ -78,7 +79,7 @@ function App() {
     }
     if (error) {
       return (
-        <div className="text-center py-10 text-red-600">
+        <div className="text-center py-10 text-red-600 dark:text-red-400">
           <h3 className="text-xl font-semibold">Error: {error}</h3>
           <p>Please try refreshing the page.</p>
         </div>
@@ -88,13 +89,13 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 transition-colors duration-200">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-200">
       {/* Header */}
-      <header className="bg-white shadow-lg sticky top-0 z-10">
+      <header className="bg-white dark:bg-gray-800 shadow-lg dark:shadow-gray-700/50 sticky top-0 z-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5">
           <div className="flex justify-between items-center">
             {/* App Title */}
-            <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-linear-to-r from-blue-600 to-purple-600 ">
+            <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-linear-to-r from-blue-600 to-purple-600 dark:from-blue-400 dark:to-purple-400">
               Contacts
             </h1>
             
@@ -102,7 +103,7 @@ function App() {
             <div className="flex items-center space-x-2">
               <button
                 onClick={() => setIsModalOpen(true)}
-                className="flex items-center space-x-2 px-4 py-2.5 bg-blue-600 text-white rounded-full font-semibold shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2  transition-all duration-200"
+                className="flex items-center space-x-2 px-4 py-2.5 bg-blue-600 text-white rounded-full font-semibold shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-900 transition-all duration-200"
               >
                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>
                 <span className="hidden sm:block">Add Contact</span>
